2ca4f45d60291c57e71a3db83d11d37f12b1b296
Mikhail V. Ireykin <ireykin@mail.ru>
2023-02-01 15:38:11 +1000

    Update first_tests.py

--- tests/first_tests.py
+++ tests/first_tests.py
@@ -6,15 +6,19 @@ class TestCalc:
     def setup(self):
         self.calc = Calculator
 
+# тест умножения
     def test_multiply_calculate_correctly(self):
         assert self.calc.multiply(self, 2, 2) == 4
-
+        
+# тест умножения
     def test_division_calculate_correctly(self):
         assert self.calc.division(self, 4, 2) == 2
-
+        
+# тест умножения
     def test_subtraction_calculate_correctly(self):
         assert self.calc.subtraction(self, 5, 2) == 3
-
+        
+# тест умножения
     def test_adding_calculate_correctly(self):
         assert self.calc.adding(self, 5, 2) == 7
 
